a2e50c6002b623b8605730e6e31f9c215fe91a00
kind-chigik <rus.work89@yandex.ru>
2019-11-07 01:34:42 +0300

    Изменено название класса и метода, определяющего статус

--- test.php
+++ test.php
@@ -1,41 +1,41 @@
 <?php
 require_once('vendor/autoload.php');
-use TaskForce\Task;
+use TaskForce\TaskStrategy;
 
 $idCustomer = '128.0.0.6';
 $idExecutor = '128.0.0.7';
 $timeEnd = '21.12.2019';
 $statusActive = 'Новое';
 
-$task = new Task ($idCustomer, $idExecutor, $timeEnd, $statusActive);
+$task = new TaskStrategy ($idCustomer, $idExecutor, $timeEnd, $statusActive);
 
-$action = Task::ACTION['cancel'];
-$status = $task->getStatus($action);
-if ($status !== Task::STATUS['cancellation']) {
+$action = TaskStrategy::ACTION['cancel'];
+$status = $task->getNextStatus($action);
+if ($status !== TaskStrategy::STATUS['cancellation']) {
     print 'Cтатус должен быть "Отменено"';
 } else {
     print 'Проверка прошла успешно ';
 }
 
-$action = Task::ACTION['accept'];
-$status = $task->getStatus($action);
-if ($status !== Task::STATUS['in_work']) {
+$action = TaskStrategy::ACTION['accept'];
+$status = $task->getNextStatus($action);
+if ($status !== TaskStrategy::STATUS['in_work']) {
     print 'Статус должен быть "В работе"';
 } else {
     print 'Проверка прошла успешно ';
 }
 
-$action = Task::ACTION['complete'];
-$status = $task->getStatus($action);
-if ($status !== Task::STATUS['completed']) {
+$action = TaskStrategy::ACTION['complete'];
+$status = $task->getNextStatus($action);
+if ($status !== TaskStrategy::STATUS['completed']) {
     print 'Статус должен быть "Выполнено"';
 } else {
     print 'Проверка прошла успешно ';
 }
 
-$action = Task::ACTION['refuse'];
-$status = $task->getStatus($action);
-if ($status !== Task::STATUS['failed']) {
+$action = TaskStrategy::ACTION['refuse'];
+$status = $task->getNextStatus($action);
+if ($status !== TaskStrategy::STATUS['failed']) {
     print 'Статус должен быть "Провалено"';
 } else {
     print 'Проверка прошла успешно ';
