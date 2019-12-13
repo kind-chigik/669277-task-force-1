e0b8dbab27754048958434dd56d3c64ac617db89
kind-chigik <rus.work89@yandex.ru>
2019-12-13 23:39:23 +0300

    Добавлен тестовый сценарий для метода getAvailableActions, внесены сопутствующие правки
    - Добавлена новая переменная для хранения id текущего пользователя.
    
    - Для переменных, содержащих id пользователей создан массив. Теперь именно его передаем в конструктор.
    
    - Добавлен тестовый сценарий для проверки метода getAvailableActions. Проверяем правильность созданных объектов в зависимости от статуса и id пользователя.

--- test.php
+++ test.php
@@ -2,16 +2,19 @@
 require_once('vendor/autoload.php');
 use TaskForce\TaskStrategy;
 
-$idCustomer = '128.0.0.6';
-$idExecutor = '128.0.0.7';
+$idUser = [
+    'idCustomer' => '128.0.0.6',
+    'idExecutor' => '128.0.0.7',
+    'idCurrentUser' => '128.0.0.7'
+];
 $timeEnd = '21.12.2019';
 $statusActive = 'Новое';
 
-$task = new TaskStrategy ($idCustomer, $idExecutor, $timeEnd, $statusActive);
+$task = new TaskStrategy ($idUser, $timeEnd, $statusActive);
 
 $action = TaskStrategy::ACTION['cancel'];
 $status = $task->getNextStatus($action);
-if ($status !== TaskStrategy::STATUS['cancellation']) {
+if ($status !== TaskStrategy::STATUS['cancelled']) {
     print 'Cтатус должен быть "Отменено"';
 } else {
     print 'Проверка прошла успешно ';
@@ -39,4 +42,50 @@ if ($status !== TaskStrategy::STATUS['failed']) {
     print 'Статус должен быть "Провалено"';
 } else {
     print 'Проверка прошла успешно ';
+}
+
+$statusNew = TaskStrategy::STATUS['new'];                    //Если статус "Новое"
+if ($idUser['idCurrentUser'] === $idUser['idExecutor']) {    //Пользователь = Исполнитель
+    $objAction = $task->getAvailableActions($statusNew);
+    var_dump($objAction);
+    $nameAction = get_class($objAction[$statusNew]);
+    if ($nameAction === TaskStrategy::ACTION['respond']) {   //Ему должно быть доступно только действие "Откликнуться"
+        print 'Объект создан верно';
+    } else {
+        print 'Объект создан неверно';
+    }
+}
+
+if ($idUser['idCurrentUser'] === $idUser['idCustomer']) {    //Пользователь = Заказчик
+    $objAction = $task->getAvailableActions($statusNew);
+    var_dump($objAction);
+    $nameAction = get_class($objAction[$statusNew]);
+    if ($nameAction === TaskStrategy::ACTION['cancel'])      {//Ему должно быть доступно действие "Отменить"
+        print 'Объект создан верно';
+    } else {
+        print 'Объект создан неверно';
+    }
+}
+
+$statusInWork = TaskStrategy::STATUS['in_work'];
+if ($idUser['idCurrentUser'] === $idUser['idExecutor']) {
+    $objAction = $task->getAvailableActions($statusInWork);
+    var_dump($objAction);
+    $nameAction = get_class($objAction[$statusInWork]);
+    if ($nameAction === TaskStrategy::ACTION['refuse']) {
+        print 'Объект создан верно';
+    } else {
+        print 'Объект создан неверно';
+    }
+}
+
+if ($idUser['idCurrentUser'] === $idUser['idCustomer']) {
+    $objAction = $task->getAvailableActions($statusInWork);
+    var_dump($objAction);
+    $nameAction = get_class($objAction[$statusInWork]);
+    if ($nameAction === TaskStrategy::ACTION['complete']) {
+        print 'Объект создан верно';
+    } else {
+        print 'Объект создан неверно';
+    }
 }
