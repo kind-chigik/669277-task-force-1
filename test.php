dd5fdfc9d6a4f3a5fa15c8f822f000cffdcc171b
kind-chigik <rus.work89@yandex.ru>
2020-01-15 03:05:29 +0300

    Исправлен код теста метода getAvailableActions
    - Удалена лишняя переменная idCurrentUser.
    
    - Значения ключей "expected" в тестируемых массивах изменены на внутренние имена классов действий.
    
    - Переписана функция для сравнения получившегося массива действий с ожидаемым.

--- test.php
+++ test.php
@@ -9,7 +9,6 @@ use TaskForce\Actions\AcceptAction;
 
 $idCustomer = 128;
 $idExecutor = 127;
-$idCurrentUser = 128;
 $timeEnd = '21.12.2019';
 $currentStatus = 'Новое';
 
@@ -47,104 +46,82 @@ if ($status !== TaskStrategy::STATUS['failed']) {
     print 'Проверка прошла успешно ';
 }
 
+// Тест метода getAvailableActions
 
 $caseNewExecutor = [                                    // Связка [Статус - Новое, Пользователь - Исполнитель]
     'currentStatus' => TaskStrategy::STATUS['new'],
     'idCurrentUser' => 127,
-    'expected' => [new RespondAction]
+    'expected' => ['respond']
 ];
 
 $caseNewCustomer = [                                    // Связка [Статус - Новое, Пользователь - Заказчик]
     'currentStatus' => TaskStrategy::STATUS['new'],
     'idCurrentUser' => 128,
-    'expected' => [new CancelAction, new AcceptAction]
+    'expected' => ['cancel', 'accept']
 ];
 
-$caseNewUnauthorized = [                                // Связка [Статус - Новое, Пользователь - Неавторизованный]
+$caseNewUnknown = [                                // Связка [Статус - Новое, Пользователь - Неизвестный]
     'currentStatus' => TaskStrategy::STATUS['new'],
     'idCurrentUser' => 129,
-    'expected' => [null]
+    'expected' => []
 ];
 
 $caseInWorkExecutor = [                                // Связка [Статус - В работе, Пользователь - Исполнитель]
     'currentStatus' => TaskStrategy::STATUS['in_work'],
     'idCurrentUser' => 127,
-    'expected' => [new RefuseAction]
+    'expected' => ['refuse']
 ];
 
 $caseInWorkCustomer = [                                // Связка [Статус - В работе, Пользователь - Заказчик]
     'currentStatus' => TaskStrategy::STATUS['in_work'],
     'idCurrentUser' => 128,
-    'expected' => [new CompleteAction]
+    'expected' => ['complete']
 ];
 
 print ('<br>' . 'Проверяем связку [Статус - Новое, Пользователь - Исполнитель]' . '<br>');
 $taskStrategy = new TaskStrategy($idCustomer, $idExecutor, $timeEnd, $caseNewExecutor['currentStatus']);
-$currentActions = $taskStrategy->getAvailableActions($caseNewExecutor['idCurrentUser']);  //Создали объекты действий для проверяемой связки
-$success = areArrayEqual($currentActions, $caseNewExecutor);
-if ($success === true) {
-    print 'Проверка прошла успешно' . '<br><br>';
-} else {
-    print 'Проверка для связки (Статус - Новое, Пользователь - Исполнитель) провалена' . '<br>';
-}
+$currentActions = $taskStrategy->getAvailableActions($caseNewExecutor['idCurrentUser']);
+$testResult = areArraysEqual($currentActions, $caseNewExecutor['expected']);
 
 print ('Проверяем связку [Статус - Новое, Пользователь - Заказчик]' . '<br>');
 $taskStrategy = new TaskStrategy($idCustomer, $idExecutor, $timeEnd, $caseNewCustomer['currentStatus']);
 $currentActions = $taskStrategy->getAvailableActions($caseNewCustomer['idCurrentUser']);
-$success = areArrayEqual($currentActions, $caseNewCustomer);
-if ($success === true) {
-    print 'Проверка прошла успешно' . '<br><br>';
-} else {
-    print 'Проверка для связки (Статус - Новое, Пользователь - Заказчик) провалена' . '<br>';
-}
+$testResult = areArraysEqual($currentActions, $caseNewCustomer['expected']);
 
-print ('Проверяем связку [Статус - Новое, Пользователь - Неавторизованный]' . '<br>');
-$taskStrategy = new TaskStrategy($idCustomer, $idExecutor, $timeEnd, $caseNewUnauthorized['currentStatus']);
-$currentActions = $taskStrategy->getAvailableActions($caseNewUnauthorized['idCurrentUser']);
-$success = areArrayEqual($currentActions, $caseNewUnauthorized);
-if ($success === null) {
-    print 'Проверка прошла успешно' . '<br><br>';
-} else {
-    print 'Проверка для связки (Статус - Новое, Пользователь - Неавторизованный) провалена' . '<br>';
-}
+print ('Проверяем связку [Статус - Новое, Пользователь - Неизвестный]' . '<br>');
+$taskStrategy = new TaskStrategy($idCustomer, $idExecutor, $timeEnd, $caseNewUnknown['currentStatus']);
+$currentActions = $taskStrategy->getAvailableActions($caseNewUnknown['idCurrentUser']);
+$testResult = areArraysEqual($currentActions, $caseNewUnknown['expected']);
 
 print ('Проверяем связку [Статус - В работе, Пользователь - Исполнитель]' . '<br>');
 $taskStrategy = new TaskStrategy($idCustomer, $idExecutor, $timeEnd, $caseInWorkExecutor['currentStatus']);
 $currentActions = $taskStrategy->getAvailableActions($caseInWorkExecutor['idCurrentUser']);
-$success = areArrayEqual($currentActions, $caseInWorkExecutor);
-if ($success === true) {
-    print 'Проверка прошла успешно' . '<br><br>';
-} else {
-    print 'Проверка для связки (Статус - Новое, Пользователь - Исполнитель) провалена' . '<br>';
-}
+$testResult = areArraysEqual($currentActions, $caseInWorkExecutor['expected']);
 
 print ('Проверяем связку [Статус - В работе, Пользователь - Заказчик]' . '<br>');
 $taskStrategy = new TaskStrategy($idCustomer, $idExecutor, $timeEnd, $caseInWorkCustomer['currentStatus']);
 $currentActions = $taskStrategy->getAvailableActions($caseInWorkCustomer['idCurrentUser']);
-$success = areArrayEqual($currentActions, $caseInWorkCustomer);
-if ($success === true) {
-    print 'Проверка прошла успешно' . '<br><br>';
-} else {
-    print 'Проверка для связки (Статус - Новое, Пользователь - Заказчик) провалена' . '<br>';
-}
-
+$testResult = areArraysEqual($currentActions, $caseInWorkCustomer['expected']);
 
 /**
  * Проверяет наличие значений одного массива в другом массиве
- *
- * @param array $currentActions Массив с проверяемыми значениями
- * @param array $сase Массив, в котором должны содержаться искомые значения
- * @return bool true если искомое значение есть в массиве, в котором происходит поиск, иначе false и выход из цикла
+ * 
+ * @param $array1 array Массив с объектами, который должны содержаться в другом массиве
+ * @param $array2 array Массив с объектами, в котором должны содержаться искомые значения
+ * 
+ * @return string Сообщение о результате проверки
  */
-
-function areArrayEqual($currentActions, $сase) {
-    foreach ($currentActions as $action) {
-        if (in_array($action, $сase['expected'])) {
-            $result = true;
-        } else {
-            $result = false;
-        break;
-        }
+function areArraysEqual($array1, $array2 = array('expected')) {
+    $nameObject = [];
+    foreach ($array1 as $key) {
+        $nameObject[] = $key::getInsideName();
     }
-    return $result;
+    $diff = array_diff($nameObject, $array2);
+    if ((empty($diff)) && (count($nameObject) === count($array2))) 
+    {
+    print 'Проверка прошла успешно' . '<br><br>';
+    } else {
+    print 'Проверка провалена' . '<br>';
+    }
+    return;
 }
