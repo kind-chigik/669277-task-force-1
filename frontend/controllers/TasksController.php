91e1362e24e1d8275876f3710af23db6c264cede
kind-chigik <rus.work89@yandex.ru>
2020-06-05 02:19:49 +0300

    Приведен к читаемому виду
    Запросы к БД убраны в модели в отдельные методы.

--- frontend/controllers/TasksController.php
+++ frontend/controllers/TasksController.php
@@ -9,11 +9,15 @@ class TasksController extends Controller
 {
     public function actionIndex()
     {
-        $query = new Query();
-        $query->select(['t.id', 't.name', 't.description', 'c.name as category_name', 'cities.city as city_name', 't.budget', 't.creation_date'])->from('tasks t')->join('INNER JOIN', 'categories c', 'category_id = c.id')->join('INNER JOIN', 'cities', 'city_id = cities.id')->where(['status' => 'new'])->orderBy(['creation_date' => SORT_DESC]);
-        $dataNewTasks = $query->all();
+        $newTasks = Tasks::find()
+        ->isNewTasks()
+        ->withCity()
+        ->withCategory()
+        ->sortByCreationDate(SORT_DESC)
+        ->all();
+
         return $this->render('index.php', [
-            'dataNewTasks' => $dataNewTasks
+            'newTasks' => $newTasks
         ]);
     }
 }
--- frontend/controllers/UsersController.php
+++ frontend/controllers/UsersController.php
@@ -9,73 +9,16 @@ class UsersController extends Controller
 {
     public function actionIndex()
     {
-        $queryExecutors = new Query();
-
-        $queryExecutors->select(['id', 'name', 'about', 'last_visit', 'rank'])->from('users')->where(['role' => 'executor'])->orderBy(['registration_date' => SORT_DESC]);
-        $executors = $queryExecutors->all();
-
-        $queryExecutorsCategories = new Query();
-        $queryExecutorsCategories->select(['u.id', 'c.name as category_name'])->from('users u')
-        ->join('INNER JOIN', 'user_categories uc', 'user_id = u.id')
-        ->join('INNER JOIN', 'categories c', 'category_id = c.id')
-        ->where(['role' => 'executor'])->orderBy(['registration_date' => SORT_DESC]);
-        $executorsCategories = $queryExecutorsCategories->all();
-
-        $executorsAndTasks = Users::find()->where(['role' => 'executor'])
-        ->joinWith('tasks0')
+        $executors = Users::find()
+        ->isExecutor()
+        ->withTasks()
+        ->withReviews()
+        ->withCategories()
+        ->sortByRegistrationDate(SORT_DESC)
         ->all();
-        $executorsAndReviews = Users::find()->where(['role' => 'executor'])
-        ->joinWith('reviews')
-        ->all();
-
-        $executorsTasks = [];
-        foreach ($executorsAndTasks as $tasks) {
-            $executorsTasks[] = [
-                'user_id' => $tasks['id'],
-                'tasks' => count($tasks['tasks0'])
-            ];
-        }
-        $executorsReviews = [];
-        foreach ($executorsAndReviews as $reviews) {
-            $executorsReviews[] = [
-                'user_id' => $reviews['id'],
-                'reviews' => count($reviews['reviews'])
-            ];
-        }
-        $tasksCount;
-        $reviewsCount;
-        $executorsAndCategories = [];
-        foreach ($executors as $executor) {
-            $categories = [];
-            foreach ($executorsCategories as $item) {
-                if ($executor['id'] === $item['id']) {
-                    $categories[] = $item['category_name'];
-                }
-            }
-            foreach($executorsTasks as $tasks) {
-                if ($tasks['user_id'] == $executor['id']) {
-                $tasksCount = $tasks['tasks'];
-                }
-            }
-            foreach($executorsReviews as $reviews) {
-                if ($reviews['user_id'] == $executor['id']) {
-                    $reviewsCount = $reviews['reviews'];
-                }
-            }
-            $executorsAndCategories[] = [
-                'id' => $executor['id'],
-                'name' => $executor['name'],
-                'about' => $executor['about'],
-                'last_visit' => $executor['last_visit'],
-                'rank' => $executor['rank'],
-                'categories_names' => $categories,
-                'tasksCount' => $tasksCount,
-                'reviewsCount' => $reviewsCount
-            ];
-        }
         
         return $this->render('index.php', [
-            'executorsAndCategories' => $executorsAndCategories,
+            'executors' => $executors
         ]);
     }
 }
